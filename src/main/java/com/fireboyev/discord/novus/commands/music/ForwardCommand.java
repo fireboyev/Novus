86525e56d66d6e54a08420502a6125fffea0bade
fireboyev <lepen2@icloud.com>
2017-09-09 22:50:28 -0500

    Fixed ForwardCommand trying to change the position of the current track when there was no track playing.

--- src/main/java/com/fireboyev/discord/novus/commands/music/ForwardCommand.java
+++ src/main/java/com/fireboyev/discord/novus/commands/music/ForwardCommand.java
@@ -31,28 +31,30 @@ public class ForwardCommand implements GuildCommandExecutor {
 	@Override
 	public void onCommand(Guild guild, User user, Member member, Message message, String[] args, MessageChannel channel,
 			MessageReceivedEvent event) {
-		if (args.length > 1) {
-			try {
-				AudioPlayer player = Main.getMusicManager().getGuildAudioPlayer(guild).player;
-				long amount = Long.parseLong(args[1]);
-				if (amount != 0) {
-					Main.getMusicManager().getGuildAudioPlayer(guild).player.getPlayingTrack()
-							.setPosition(player.getPlayingTrack().getPosition() + (amount * 1000));
-					if (amount > 0)
-						channel.sendMessage("Skipped Forward " + amount + " Second(s)").queue();
-					else
-						channel.sendMessage("Skipped Backward " + -amount + " Second(s)").queue();
-				} else
-					channel.sendMessage(
-							"Hey there, I'm sorry, but currently space and time doesn't allow you to skip forward 0 seconds.")
-							.queue();
-			} catch (NumberFormatException e) {
-				channel.sendMessage("Oi, I would prefer if it was a number!").queue();
+		AudioPlayer player = Main.getMusicManager().getGuildAudioPlayer(guild).player;
+		if (player.getPlayingTrack() != null) {
+			if (args.length > 1) {
+				try {
+					long amount = Long.parseLong(args[1]);
+					if (amount != 0) {
+						Main.getMusicManager().getGuildAudioPlayer(guild).player.getPlayingTrack()
+								.setPosition(player.getPlayingTrack().getPosition() + (amount * 1000));
+						if (amount > 0)
+							channel.sendMessage("Skipped Forward " + amount + " Second(s)").queue();
+						else
+							channel.sendMessage("Skipped Backward " + -amount + " Second(s)").queue();
+					} else
+						channel.sendMessage(
+								"Hey there, I'm sorry, but currently space and time doesn't allow you to skip forward 0 seconds.")
+								.queue();
+				} catch (NumberFormatException e) {
+					channel.sendMessage("Oi, I would prefer if it was a number!").queue();
+				}
+			} else {
+				channel.sendMessage("How many seconds? Negative numbers work too!").queue();
 			}
-		} else {
-			channel.sendMessage("How many seconds? Negative numbers work too!").queue();
-		}
 
+		}else
+			channel.sendMessage("I could try making up my own music, but I wouldn't be very good at it... (There is no track playing currently!)").queue();
 	}
-
 }
